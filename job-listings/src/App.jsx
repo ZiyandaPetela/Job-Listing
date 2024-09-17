6288807752785e16dc12099fcaee9761277d7b80
Oladimeji <oladimejimatthew433@gmail.com>
2024-09-17 23:02:55 +0100

    added custom colors for tailwindCss and also included BrowserRouter

--- job-listings/src/App.jsx
+++ job-listings/src/App.jsx
@@ -1,8 +1,4 @@
-
-
-
-
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
--- job-listings/tailwind.config.js
+++ job-listings/tailwind.config.js
@@ -1,20 +1,20 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "350px",
+      },
+      colors: {
+        primary: "#4423A9",
+        secondary: "#6A44D9",
+        accent: "#E3DBFA",
+        dark: "#141413",
+        light: "#F5F5F5",
+        yellow: "#FFD62C",
+      },
+    },
   },
   plugins: [],
-}
+};
