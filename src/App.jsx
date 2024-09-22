8f83f9f1ca98a0c5d2c5087e2da4f57e58737a2d
uvyne-rop <ropchiri7@gmail.com>
2024-09-22 14:37:53 +0300

    added client-side routing for the employee profile

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import EmployeeProfileForm from "../src/Components/EmployeeProfileForm"; // Import EmployeeProfileForm from employees.jsx
-import EmployerProfileForm from "../src/Components/EmployerProfileForm";
+
 
 function App() {
   return (
