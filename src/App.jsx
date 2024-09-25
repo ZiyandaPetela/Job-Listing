2c6afd253a90911bd3028b7b4468606637587df4
uvyne-rop <ropchiri7@gmail.com>
2024-09-25 09:55:14 +0300

    made schanges

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import EmployerProfileForm from "./Components/EmployerProfileForm";
+import EmployeeProfileForm from "./Components/EmployeeProfileForm"; // Add Employee Profile Form
+import LandingPage from "./Components/LandingPage"; // Add Landing Page
+import JobListingPage from "./Components/JobListingPage"; // Add Job Listing Page
+
 function App() {
   return (
-    <>
-     <EmployerProfileForm/>
-    </>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/employer-profile">Employer Profile</Link>
+          </li>
+          <li>
+            <Link to="/employee-profile">Employee Profile</Link>
+          </li>
+          <li>
+            <Link to="/job-listings">Job Listings</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        {/* Route for Landing Page */}
+        <Route path="/" element={<LandingPage />} />
+
+        {/* Route for Employer Profile Form */}
+        <Route path="/employer-profile" element={<EmployerProfileForm />} />
+
+        {/* Route for Employee Profile Form */}
+        <Route path="/employee-profile" element={<EmployeeProfileForm />} />
+
+        {/* Route for Job Listing Page */}
+        <Route path="/job-listings" element={<JobListingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
