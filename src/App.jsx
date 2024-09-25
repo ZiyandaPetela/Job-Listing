d73261e68615911f25339858b3e66a6454f05a6a
Oladimeji <oladimejimatthew433@gmail.com>
2024-09-25 15:07:27 +0100

    handled the job listing portal UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 /* eslint-disable no-unused-vars */
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Homepage from "../src/Pages/Homepage"
-import JobListingPage from "./Pages/JobListingPage/JobListingPage";
+import Homepage from "../src/Pages/Homepage";
 function App() {
   return (
     <>
-     {/* <Homepage/> */}
-     <JobListingPage />
+      <Homepage />
     </>
   );
 }
