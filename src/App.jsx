b185b363862d09ec04ad02703cd649f4aa6e7331
uvyne-rop <ropchiri7@gmail.com>
2024-09-25 10:44:22 +0300

    changed the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import EmployerProfileForm from "./Components/EmployerProfileForm";
-import EmployeeProfileForm from "./Components/EmployeeProfileForm"; // Add Employee Profile Form
-import LandingPage from "./Components/LandingPage"; // Add Landing Page
-import JobListingPage from "./Components/JobListingPage"; // Add Job Listing Page
+import Homepage from "./Pages/Homepage";
+import EmployeeProfileForm from "../src/Components/EmployeeProfileForm"; // Import EmployeeProfileForm from employees.jsx
+import EmployerProfileForm from "../src/Components/EmployerProfileForm"; // Import EmployeeProfileForm from employees.jsx
+
 
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/employer-profile">Employer Profile</Link>
-          </li>
-          <li>
-            <Link to="/employee-profile">Employee Profile</Link>
-          </li>
-          <li>
-            <Link to="/job-listings">Job Listings</Link>
-          </li>
-        </ul>
-      </nav>
-
       <Routes>
-        {/* Route for Landing Page */}
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<EmployeeProfileForm/>} />
+        <Route path="/" element={<EmployerProfileForm/>} />
 
-        {/* Route for Employer Profile Form */}
-        <Route path="/employer-profile" element={<EmployerProfileForm />} />
-
-        {/* Route for Employee Profile Form */}
-        <Route path="/employee-profile" element={<EmployeeProfileForm />} />
-
-        {/* Route for Job Listing Page */}
-        <Route path="/job-listings" element={<JobListingPage />} />
+        
       </Routes>
     </Router>
   );
