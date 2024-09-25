d71cbc2d5bd3685fcbe38024c97f867a77ae97e0
uvyne-rop <ropchiri7@gmail.com>
2024-09-25 10:34:08 +0300

    added the client side route to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-
+/* eslint-disable no-unused-vars */
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
-import Application from "./Pages/Application";
-import EmployeeSignUpPage from "./Pages/EmployeeSignUpPage";
-import EmployeeLoginpage from "./Pages/EmployeeLoginpage";
-
 import EmployeeProfileForm from "../src/Components/EmployeeProfileForm"; // Import EmployeeProfileForm from employees.jsx
 import EmployerProfileForm from "../src/Components/EmployerProfileForm"; // Import EmployeeProfileForm from employees.jsx
 
@@ -15,12 +11,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
-      
-        <Route path="/EmployeeLoginpage" element={<EmployeeLoginpage />} />
-        <Route path="/EmployeeSignupPage" element={<EmployeeSignUpPage />} />
-        <Route path="/apply" element={<Application/>} />
-        <Route path="/EmployeeProfileForm" element={<EmployeeProfileForm/>} />
-        <Route path="/EmployerProfileForm" element={<EmployerProfileForm/>} />
+        <Route path="/" element={<EmployeeProfileForm/>} />
+        <Route path="/" element={<EmployerProfileForm/>} />
 
         
       </Routes>
