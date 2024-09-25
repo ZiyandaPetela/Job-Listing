04ba83ba13f14358ded062b3a2faa9be60078f43
uvyne-rop <ropchiri7@gmail.com>
2024-09-25 10:32:20 +0300

    modified the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,10 @@ import EmployeeProfileForm from "../src/Components/EmployeeProfileForm"; // Impo
 function App() {
   return (
     <Router>
-      <div className="bg-gray-100 min-h-screen">
-        <header className="bg-blue-500 p-4">
-          
-          <nav>
-            <ul className="flex space-x-4 mt-2">
-              <li>
-                <Link to="/" className="text-white">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to="/employee-profile" className="text-white">
-                  Employee Profile
-                </Link>
-              </li>
-              
-            </ul>
-          </nav>
-        </header>
-
-        {/* Define Routes */}
-        <Routes>
-          <Route path="/" element={<h2 className="text-center mt-8">Welcome to the Job Listing Project</h2>} />
-          <Route path="/employee-profile" element={<EmployeeProfileForm />} />
-         
-        </Routes>
+      <Routes>
+        <Route path="/" element={<EmployeeProfileForm/>} />
         
-      </div>
+      </Routes>
     </Router>
   );
 }
